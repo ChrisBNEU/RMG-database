96ce92e1d44aab6029b2a631605dcd4ca86400dc
Hao-Wei Pang <hwpang@rmg.mit.edu>
2020-04-27 13:33:43 -0400

    Added a rate rule in 2+2_cycloaddition_Cd/rules.py
    The rate was taken from its own training reaction

--- input/kinetics/families/2+2_cycloaddition_Cd/rules.py
+++ input/kinetics/families/2+2_cycloaddition_Cd/rules.py
@@ -4,5 +4,20 @@
 name = "2+2_cycloaddition_Cd/rules"
 shortDesc = u""
 longDesc = u"""
-
+Taken from training reactions.py
 """
+entry(
+    index = 1,
+    label = "db;doublebond",
+    degeneracy = 2.0,
+    kinetics = ArrheniusEP(
+        A = (4.66e+06, 'cm^3/(mol*s)', '*|/', 5),
+        n = 1.65,
+        alpha = 0,
+        E0 = (226.564, 'kJ/mol'),
+        Tmin = (300, 'K'),
+        Tmax = (1500, 'K'),
+    ),
+    rank = 0,
+)
+
