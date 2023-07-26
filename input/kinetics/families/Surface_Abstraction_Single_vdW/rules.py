29476b05cf730fdddc09883aef0a17a3a9df109b
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2023-07-26 11:01:12 -0400

    adding additional rules from uncertainty rule generation

--- input/kinetics/families/Surface_Abstraction_Single_vdW/rules.py
+++ input/kinetics/families/Surface_Abstraction_Single_vdW/rules.py
@@ -2,24 +2,33 @@
 # encoding: utf-8
 
 name = "Surface_Abstraction_Single_vdW/rules"
-shortDesc = u""
-longDesc = u"""
+shortDesc = ""
+longDesc = """
 A vdW species splitting, adsorbing to the surface, 
 and transferring a functional group to a single bonded surface species.
 """
-
 entry(
     index = 1,
     label = "Donating;Abstracting",
-    kinetics = SurfaceArrheniusBEP(
-        A = (1.0e13, 'm^2/(mol*s)'),
-        n = 0,
-        alpha = 0.5,
-        E0 = (0, 'kcal/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
+    kinetics = SurfaceArrheniusBEP(A=(1e+13,'m^2/(mol*s)'), n=0, alpha=0.5, E0=(0,'kcal/mol'), Tmin=(200,'K'), Tmax=(3000,'K')),
     rank = 0,
-    shortDesc = u"""Default""",
-    longDesc = u"""Made up"""
+    shortDesc = """Default""",
+    longDesc = 
+"""
+Made up
+""",
 )
+
+entry(
+    index = 2,
+    label = "C-OH;*C=R",
+    kinetics = SurfaceArrheniusBEP(A=(1e+17,'cm^2/(mol*s)'), n=0, alpha=0.5, E0=(0,'kcal/mol')),
+    rank = 1,
+    shortDesc = """Rate rule generated for uncertainty""",
+    longDesc = 
+"""
+Rate rule generated for uncertainty
+""",
+)
+
+
--- input/kinetics/families/Surface_Abstraction_vdW/rules.py
+++ input/kinetics/families/Surface_Abstraction_vdW/rules.py
@@ -2,24 +2,32 @@
 # encoding: utf-8
 
 name = "Surface_Abstraction_vdW/rules"
-shortDesc = u""
-longDesc = u"""
+shortDesc = ""
+longDesc = """
 A vdW double bonded species dissociatively adsorbing to the surface with double
 bonds.
 """
-
 entry(
     index = 1,
     label = "AdsorbateVdW;Adsorbate1",
-    kinetics = SurfaceArrheniusBEP(
-        A = (1.0e13, 'm^2/(mol*s)'),
-        n = 0,
-        alpha = 0.5,
-        E0 = (0, 'kcal/mol'),
-        Tmin = (200, 'K'),
-        Tmax = (3000, 'K'),
-    ),
+    kinetics = SurfaceArrheniusBEP(A=(1e+13,'m^2/(mol*s)'), n=0, alpha=0.5, E0=(0,'kcal/mol'), Tmin=(200,'K'), Tmax=(3000,'K')),
     rank = 0,
-    shortDesc = u"""Default""",
-    longDesc = u"""Made up"""
+    shortDesc = """Default""",
+    longDesc = 
+"""
+Made up
+""",
 )
+
+entry(
+    index = 2,
+    label = "C-OH;*=C=R",
+    kinetics = SurfaceArrheniusBEP(A=(1e+17,'cm^2/(mol*s)'), n=0, alpha=0.5, E0=(2.707,'kcal/mol')),
+    rank = 1,
+    shortDesc = """Rate rule generated for uncertainty""",
+    longDesc = 
+"""
+Rate rule generated for uncertainty
+""",
+)
+
